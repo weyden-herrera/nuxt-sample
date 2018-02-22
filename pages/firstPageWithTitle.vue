4b83a9d1648a088aba00deb01db09d1f2acff031
Weyden Herrera <weydenherrera@Dunlin.local>
2018-02-22 15:48:35 +0800

    initial commit

---
+++ pages/firstPageWithTitle.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model= "email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model= "password">
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model= "checkMeOut">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button @click.prevent= "onSubmit" type="button" class="btn btn-primary">Submit</button>
+        </form>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  data(){
+      return {
+          title: 'Sample title',
+          email: '',
+          password: '',
+          checkMeOut: false
+      }
+  },
+  methods: {
+      onSubmit() {
+          //jQuery way
+          // let test = jQuery('#exampleInputEmail1').val();
+
+          //vue way
+          let email = this.email;
+          let password = this.password;
+          let checkMeOut = this.checkMeOut;
+      }
+  }
+}
+</script>
---
+++ pages/listExample.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <h2>Basic List Group</h2>
+    <ul class="list-group">
+        <!-- this code currently has a problem with vetur :( -->
+        <!-- <li v-for= "item in exampleList" >
+            {item.name}
+        </li> -->
+        
+    </ul>
+  </section>
+</template>
+
+<script>
+
+    export default {
+        components: {
+            AppLogo
+        },
+        data(){
+            return {
+                exampleList: [
+                    {
+                        name: 'Weyden',
+                        email: 'weydenherrera@gmail.com'
+                    },
+                    {
+                        name: 'Andy',
+                        email: 'andy.dyems@gmail.com'
+                    }
+                ]
+            }
+        }
+    }
+
+</script>
